bed7ec10fa8aa999a66d7c75ab81f62533fbc278
Dean Johnson <dean@deanljohnson.com>
2018-12-20 23:10:49 -0800

    Added a Stack implementation.

---
+++ SimuNet/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimuNet
+{
+    /// <summary>
+    /// Simulates a programs stack space.
+    /// </summary>
+    public class Stack
+    {
+        private readonly int[] m_Stack;
+
+        public int this[int address]
+        {
+            get
+            {
+                if (address < 0)
+                    throw new InvalidOperationException("Stack Underflow");
+
+                return m_Stack[address];
+            }
+            set
+            {
+                if (address >= m_Stack.Length)
+                    throw new InvalidOperationException("Stack Overflow");
+                m_Stack[address] = value;
+            }
+        }
+
+        public Stack(int size)
+        {
+            m_Stack = new int[size];
+        }
+    }
+}

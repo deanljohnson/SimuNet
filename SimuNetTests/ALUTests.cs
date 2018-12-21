d7c639d994ae2d413c16d87b17696817efc22353
Dean Johnson <dean@deanljohnson.com>
2018-12-21 13:50:26 -0800

    Rewrote ALUTests to reduce code reuse.

--- SimuNetTests/ALUTests.cs
+++ SimuNetTests/ALUTests.cs
@@ -8,69 +8,53 @@ namespace SimuNetTests
     public class ALUTests
     {
         [TestMethod]
-        public void DoOpIntTest()
+        public void DoOpErrorTest()
         {
             ALU alu = new ALU();
-
-            alu.DoOp(OpCode.Add, 5, 7, out int addPosResult);
-            Assert.AreEqual(addPosResult, 12);
-            alu.DoOp(OpCode.Add, -5, -7, out int addNegResult);
-            Assert.AreEqual(addNegResult, -12);
-            alu.DoOp(OpCode.Add, -5, 7, out int addMixedResult);
-            Assert.AreEqual(addMixedResult, 2);
-            alu.DoOp(OpCode.Add, -5, 0, out int addZeroResult);
-            Assert.AreEqual(addZeroResult, -5);
-            alu.DoOp(OpCode.Add, 0, 0, out int addNoneResult);
-            Assert.AreEqual(addNoneResult, 0);
-
-            alu.DoOp(OpCode.Sub, 3, 10, out int subPosResult);
-            Assert.AreEqual(subPosResult, -7);
-            alu.DoOp(OpCode.Sub, -3, -10, out int subNegResult);
-            Assert.AreEqual(subNegResult, 7);
-            alu.DoOp(OpCode.Sub, -3, 10, out int subMixedResult);
-            Assert.AreEqual(subMixedResult, -13);
-            alu.DoOp(OpCode.Sub, -5, 0, out int subZeroResult);
-            Assert.AreEqual(subZeroResult, -5);
-            alu.DoOp(OpCode.Sub, 0, 0, out int subNoneResult);
-            Assert.AreEqual(subNoneResult, 0);
-
-            alu.DoOp(OpCode.Mul, 5, 7, out int mulPosResult);
-            Assert.AreEqual(mulPosResult, 35);
-            alu.DoOp(OpCode.Mul, -5, -7, out int mulNegResult);
-            Assert.AreEqual(mulNegResult, 35);
-            alu.DoOp(OpCode.Mul, -5, 7, out int mulMixedResult);
-            Assert.AreEqual(mulMixedResult, -35);
-            alu.DoOp(OpCode.Mul, -5, 0, out int mulZeroResult);
-            Assert.AreEqual(mulZeroResult, 0);
-            alu.DoOp(OpCode.Mul, 0, 0, out int mulNoneResult);
-            Assert.AreEqual(mulNoneResult, 0);
-            
-            alu.DoOp(OpCode.Div, 10, 2, out int divPosResult);
-            Assert.AreEqual(divPosResult, 5);
-            alu.DoOp(OpCode.Div, -10, -2, out int divNegResult);
-            Assert.AreEqual(divNegResult, 5);
-            alu.DoOp(OpCode.Div, -10, 5, out int divMixedResult);
-            Assert.AreEqual(divMixedResult, -2);
-            alu.DoOp(OpCode.Div, -5, 0, out int divZeroResult);
-            Assert.AreEqual(divZeroResult, 0);
-            Assert.AreEqual(alu.Error, ALU.ErrorCode.DivisionByZero);
-            alu.DoOp(OpCode.Div, 0, 0, out int divNoneResult);
-            Assert.AreEqual(divNoneResult, 0);
-            Assert.AreEqual(alu.Error, ALU.ErrorCode.DivisionByZero);
-
-            alu.DoOp(OpCode.LeftShift, 1, 1, out int leftShiftResult);
-            Assert.AreEqual(2, leftShiftResult);
-            alu.DoOp(OpCode.RightShift, 2, 1, out int rightShiftResult);
-            Assert.AreEqual(1, rightShiftResult);
-            alu.DoOp(OpCode.LeftShiftI, 1, 1, out int leftShiftIResult);
-            Assert.AreEqual(2, leftShiftIResult);
-            alu.DoOp(OpCode.RightShiftI, 2, 1, out int rightShiftIResult);
-            Assert.AreEqual(1, rightShiftIResult);
-
-            alu.DoOp(OpCode.NoOp, 1, 1, out int noOpResult);
-            Assert.AreEqual(noOpResult, 0);
-
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => alu.DoOp(OpCode.Error, 0, 0, out int _));
+        }
+
+        [DataTestMethod]
+        [DataRow(OpCode.NoOp, 1, 1, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+
+        [DataRow(OpCode.Add, 5, 7, 12, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Add, -5, -7, -12, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Add, -5, 7, 2, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Add, -5, 0, -5, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Add, 0, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+
+        [DataRow(OpCode.Sub, 5, 7, -2, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Sub, -5, -7, 2, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Sub, -5, 7, -12, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Sub, -5, 0, -5, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Sub, 0, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+
+        [DataRow(OpCode.Mul, 5, 7, 35, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Mul, 5, -7, -35, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Mul, -5, -7, 35, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Mul, 5, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Mul, 0, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+
+        [DataRow(OpCode.Div, 10, 2, 5, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Div, -10, -2, 5, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Div, -10, 2, -5, ALU.Flags.Negative, ALU.ErrorCode.None)]
+        [DataRow(OpCode.Div, -10, 0, 0, ALU.Flags.None, ALU.ErrorCode.DivisionByZero)]
+        [DataRow(OpCode.Div, 0, 0, 0, ALU.Flags.None, ALU.ErrorCode.DivisionByZero)]
+
+        [DataRow(OpCode.LeftShift, 1, 1, 2, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.LeftShift, 1, 0, 1, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.LeftShift, 0, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+
+        [DataRow(OpCode.RightShift, 1, 1, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+        [DataRow(OpCode.RightShift, 1, 0, 1, ALU.Flags.Positive, ALU.ErrorCode.None)]
+        [DataRow(OpCode.RightShift, 0, 0, 0, ALU.Flags.Zero, ALU.ErrorCode.None)]
+        public void DoOpTest(OpCode op, int a, int b, int expectedResult, ALU.Flags flags, ALU.ErrorCode error)
+        {
+            ALU alu = new ALU();
+            alu.DoOp(op, a, b, out int actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(flags, alu.StatusFlags);
+            Assert.AreEqual(error, alu.Error);
         }
     }
 }

6b5e14ca5a4a7debd300e6899633623fb44473b1
Dean Johnson <jdean300@yahoo.com>
2019-01-23 21:18:33 -0800

    Added basic test for multi file programs.

--- SimuNetAssemblerTests/AssemblerTests.cs
+++ SimuNetAssemblerTests/AssemblerTests.cs
@@ -29,5 +29,29 @@ namespace SimuNetAssembler.Tests
             Assert.Equal(OpCode.Add, prog[1].Code);
             Assert.Equal(OpCode.Exit, prog[2].Code);
         }
+
+        [Fact]
+        public void MultiFileTest()
+        {
+            CPU cpu = new CPU(new Memory(65536));
+            Assembler assem = new Assembler(cpu);
+            assem.BeginProgram();
+
+            assem.Assemble(new FileInfo("Programs/multi-file-1.txt"));
+            assem.Assemble(new FileInfo("Programs/multi-file-2.txt"));
+
+            Program prog = assem.EndProgram();
+            Assert.Equal(5, prog.InstructionCount);
+            Assert.Equal(OpCode.LoadI, prog[0].Code);
+            Assert.Equal(OpCode.Jump, prog[1].Code);
+            Assert.Equal(OpCode.Exit, prog[2].Code);
+            Assert.Equal(OpCode.Add, prog[3].Code);
+            Assert.Equal(OpCode.Exit, prog[4].Code);
+
+            cpu.LoadProgram(prog);
+            cpu.RunProgram();
+
+            Assert.Equal(10, cpu.V0.Value);
+        }
     }
 }
